10c543523eee96d57d7e19d1748c0894e982f987
venkat70756 <venkatyvry1234@gmail.com>
2026-08-16 16:12:23 +0530

    updated header block created

---
+++ blocks/updated-header/updated-header.js
@@ -0,0 +1,37 @@
+
+export default async function decorate(block) {
+  const row = block.children[0];
+
+  const logo = row.children[0];
+  const company = row.children[1];
+  const about = row.children[2];
+  const contact = row.children[3];
+  const login = row.children[4];
+
+
+  console.log('Logo:', logo);
+  console.log('Company:', company);
+  console.log('About:', about);
+  console.log('Contact:', contact);
+  console.log('Login:', login);
+
+
+  logo.classList.add('header-logo');
+  company.classList.add('header-company');
+  about.classList.add('header-link');
+  contact.classList.add('header-link');
+  login.classList.add('header-link');
+
+
+  const left = document.createElement('div');
+  left.classList.add('header-left');
+
+  const right = document.createElement('nav');
+  right.classList.add('header-right');
+
+  left.append(logo, company);
+  right.append(about, contact, login);
+
+  row.append(left, right);
+
+}
